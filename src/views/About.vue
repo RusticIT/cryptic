978cb340b9804a238a7997149822d168b2cbfd53
Saulo Vargas <sauloco022@email.com>
2021-02-11 15:54:10 -0300

    Introducción y Setup de Vue Router

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Some dummy about</h1>
+    <router-link to="/"> Volver al inicio </router-link>
+  </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Error 404</h1>
+    <router-link to="/">Volver al inicio</router-link>
+  </div>
+</template>
